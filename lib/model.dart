76f0df18d3c4c74ab22f109741e10e7374eb18e7
Udit Swaroopa <65483816+uditswaroopa@users.noreply.github.com>
2020-09-30 23:20:30 +0530

    Added equalsTo and BaseConversion method

--- lib/model.dart
+++ lib/model.dart
@@ -3,4 +3,27 @@ class Model {
   String history;
 
   Model(this.expression, this.history);
+  
+  void equalsTo(){
+    expression = expression.replaceAll('MOD','%');
+    expression = expression.replaceAll('LOG','log10');
+    print(expression);
+    try{
+      Parser p = Parser();
+      Expression exp = p.parse(expression);
+      ContextModel cm = ContextModel();
+      double eval = exp.evaluate(EvaluationType.REAL, cm);
+      expression = eval.toString();
+    }catch(e){
+      expression = 'Incorrect Syntax';
+      history =''; 
+    }
+  }
+  
+  void baseConversion(int base){
+    print('demo');
+    history = expression;
+    expression = int.parse(expression).toRadixString(base);
+  }
+
 }
