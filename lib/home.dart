a2b1a30ecf2bcba5fb1806766400552a2d462a28
Udit Swaroopa <65483816+uditswaroopa@users.noreply.github.com>
2020-09-30 23:21:32 +0530

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -12,19 +12,22 @@ class _CalcAppState extends State<CalcApp> {
   Model object = new Model('', ''); //creating object of class Model
 
   tap(String x) {
-    if (x == 'e') x = 'e^';
+    if(x == 'e')
+      x = 'e^';
 
-    if (x == 'D to B' || x == 'D to O' || x == 'D to H') {
-      if (x == 'D to B')
-        print('base conversion 2');
-      else if (x == 'D to O')
-        print('base conversion 8');
-      else if (x == 'D to H') print('base conversion 16');
+    if(x == 'D to B' || x == 'D to O' || x == 'D to H'){
+      if(x == 'D to B')
+        object.baseConversion(2);  
+      else if(x == 'D to O')
+        object.baseConversion(8);  
+      else if(x == 'D to H')
+        object.baseConversion(16);  
       x = '';
     }
 
     setState(() {
-      if (object.expression == 'Incorrect Syntax') object.expression = '';
+      if(object.expression == 'Incorrect Syntax')
+        object.expression = '';
       object.expression += x;
     });
   }
@@ -36,10 +39,10 @@ class _CalcAppState extends State<CalcApp> {
     });
   }
 
-  equal(String x) {
+  equal(String x){
     setState(() {
       object.history = object.expression;
-      print('equals too');
+      object.equalsTo();
     });
   }
 
@@ -51,7 +54,7 @@ class _CalcAppState extends State<CalcApp> {
         children: <Widget>[
           Expanded(
             flex: 3,
-            child: LCD(object), //object passed to LCD widget
+            child: LCD(object),//object passed to LCD widget
           ),
           Expanded(
             flex: 7,
@@ -69,64 +72,64 @@ class _CalcAppState extends State<CalcApp> {
                 //represent column2
                 Row(
                   children: <Widget>[
-                    Button('LOG', tap),
-                    Button('(', tap),
-                    Button(')', tap),
-                    Button('OR', tap),
+                    Button('LOG',tap),
+                    Button('(',tap),
+                    Button(')',tap),
+                    Button('OR',tap),
                   ],
                 ),
                 //represent column3
                 Row(
                   children: <Widget>[
-                    Button('^', tap),
-                    Button('', tap),
-                    Button('e', tap),
-                    Button('XOR', tap),
+                    Button('^',tap),
+                    Button('',tap),
+                    Button('e',tap),
+                    Button('XOR',tap),
                   ],
                 ),
                 //represent column4
                 Row(
                   children: <Widget>[
-                    Button('sin', tap),
-                    Button('cos', tap),
-                    Button('tan', tap),
-                    Button('MOD', tap),
+                    Button('sin',tap),
+                    Button('cos',tap),
+                    Button('tan',tap),
+                    Button('MOD',tap),
                   ],
                 ),
                 //represent column5
                 Row(
                   children: <Widget>[
-                    Button('D to B', tap),
-                    Button('D to O', tap),
-                    Button('D to H', tap),
-                    Button('*', tap),
+                    Button('D to B',tap),
+                    Button('D to O',tap),
+                    Button('D to H',tap),
+                    Button('*',tap),
                   ],
                 ),
                 //represent column6
                 Row(
                   children: <Widget>[
-                    Button('7', tap),
-                    Button('8', tap),
-                    Button('9', tap),
-                    Button('/', tap),
+                    Button('7',tap),
+                    Button('8',tap),
+                    Button('9',tap),
+                    Button('/',tap),
                   ],
                 ),
                 //represent column7
                 Row(
                   children: <Widget>[
-                    Button('4', tap),
-                    Button('5', tap),
-                    Button('6', tap),
-                    Button('+', tap),
+                    Button('4',tap),
+                    Button('5',tap),
+                    Button('6',tap),
+                    Button('+',tap),
                   ],
                 ),
                 //represent column8
                 Row(
                   children: <Widget>[
-                    Button('1', tap),
-                    Button('2', tap),
-                    Button('3', tap),
-                    Button('-', tap),
+                    Button('1',tap),
+                    Button('2',tap),
+                    Button('3',tap),
+                    Button('-',tap),
                   ],
                 ),
                 //represent column9
